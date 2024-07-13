49780546659ca0b10b625106211fad5a4daf3422
Sukhbir Singh <89540293+sukhbirforservice@users.noreply.github.com>
2024-07-14 02:41:14 +0530

    Add files via upload

---
+++ Day 1 Variables and Data Types.js
@@ -0,0 +1,40 @@
+// Activity 1
+var dob = 2002;
+console.log(dob);
+
+let whosdob = "Honey's date of birth";
+console.log(whosdob);
+
+// Activity 2
+const boolean = false;
+console.log(boolean);
+
+// Activity 3
+
+let object = {
+	name: "Honey",
+	info: "Date of Birth",
+	place: "Jamshedpur"
+}
+
+let array = [2, 3, "numbers", true];
+
+console.log(typeof dob);
+console.log(typeof whosdob);
+console.log(typeof boolean);
+console.log(typeof object);
+console.log(typeof array);
+
+// Activity 4 
+
+// let number = 27;
+// let number = 22;
+// console.log(number);
+
+// Uncaught SyntaxError: Identifier 'number' has already been declared (at Day 1 Variables and Data Types.js:31:5)
+
+// Activity 5 Understanding const
+
+const boolean = true;
+
+// Uncaught SyntaxError: Identifier 'boolean' has already been declared (at Day 1 Variables and Data Types.js:38:7)
