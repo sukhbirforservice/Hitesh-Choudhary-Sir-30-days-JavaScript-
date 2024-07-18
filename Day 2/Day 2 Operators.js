559c7110eb7be65305715f3a8e674afafe619b7b
Sukhbir Singh <89540293+sukhbirforservice@users.noreply.github.com>
2024-07-18 18:51:22 +0530

    Add files via upload

---
+++ Day 2/Day 2 Operators.js
@@ -0,0 +1,87 @@
+// Activity 1: Arithmetic Operations
+// Task 1
+var dob = 2002;
+var herdob = 2003
+var bothdob = dob + herdob;
+console.log(bothdob);
+
+// Task 2
+
+var bothdob = herdob - dob;
+console.log(bothdob);
+
+// Q. When I am using var it logs the result but when I use let it didn't.
+
+// Task 3
+
+var multidob = herdob * dob;
+console.log(multidob);
+
+// Task 4
+
+var dividedob = herdob/dob;
+console.log(dividedob);
+
+// Task 5
+ 
+var remaider = herdob%dob; 
+console.log(remaider);
+
+// Activity 2: Assignment Operators 
+
+// Task 6
+
+var count = 0;
+//var count += dob; 
+console.log(count);
+
+// Task 7
+var count = 0;
+//var count -= dob; 
+console.log(count);
+
+
+// Activity 3: Comparison Operators
+
+// Task 8
+ 
+var compare = dob > herdob;
+console.log(compare);
+ 
+var compare = dob < herdob;
+console.log(compare);
+
+// Task 9
+
+var check = 2002 <= 2001;
+console.log(check);
+
+var check = 2002 >= 2001;
+console.log(check);
+
+
+// Task 10 
+
+var equalcheck = 2002 == "2002";
+console.log(equalcheck);
+
+
+var strictlycheck = 2002 === "2002";
+console.log(strictlycheck);
+
+// Activity 4: Logical Operators
+
+var both = herdob && dob == 0;
+console.log(both); 
+
+var notequal = 5 != null;
+console.log(notequal);
+
+// Activity 5 Ternary Operator
+
+5 < 23 ? console.log("greater"): console.log("Not greater") 
+
+
+
+
+
